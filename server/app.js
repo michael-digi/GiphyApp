db05240bb0379b842fd4e03c9b7d27fec57f5dbc
Michael <michael.digiorgio70@qmail.cuny.edu>
2020-07-07 16:54:04 -0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,26 @@
+// import express (after npm install express)
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet')
+// create new express app and save it as "app"
+const app = express();
+
+// server configuration
+const PORT = 8080;
+
+app.use(helmet())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
+app.use(express.json());
+
+// create a route for the app
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+}); 
